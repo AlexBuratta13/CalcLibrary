6a6a37c0b7fb95a62b5433114352c04de76903b9
AlexBuratta13 <alex,buratta13@gmail.com>
2020-01-09 17:56:29 +0100

    Add project files.

---
+++ CalcLibrary.Test/CalcTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalcLibrary.Test
+{
+    [TestClass]
+    public class CalcTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
